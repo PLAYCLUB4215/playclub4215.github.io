e31f1c9245f0677523dbf367c79633315da19539
playclub4215 <zangunknow42@gmail.com>
2026-06-19 14:00:35 +0800

    chore: set site URL to https://playclub4215.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://minzang.github.io',
+  site: 'https://playclub4215.github.io',
   // User Page 根網址，base 維持預設 '/'
 });
